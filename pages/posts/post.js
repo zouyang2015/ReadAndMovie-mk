6d286255f05c51c2472adde85feec0f1ebadb34c
zouyang@baokeyun.com <86022310@qq.com>
2018-07-12 23:13:41 +0800

    first

---
+++ pages/posts/post.js
@@ -0,0 +1,33 @@
+// pages/posts/posts.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgUrls: [
+      '/image/post/main_01.jpg',
+      '/image/post/main_02.jpg',
+      '/image/post/main_03.jpg',
+      '/image/post/main_04.jpg',
+      '/image/post/main_05.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
